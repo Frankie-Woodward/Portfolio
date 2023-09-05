f63aff69a886aadfeff3b1fd4e86f67200b23886
Frankie <fwoodwar@gmail.com>
2023-09-05 15:10:26 -0600

    cloned my personal github repository. added html, css, and js main files. Also started creating content for index.html

---
+++ functions.js
@@ -0,0 +1,10 @@
+
+
+// Nav menu pop-out or scroll down from button click
+
+// carousel of project pics with information container 
+// toggle for dark mode background
+// Message to email box
+// Comment box that displays in a thread
+// carousel of cool pics with information container 
+// click-able image to enter site
