3cc685b2bd8f5c21e367e8daffe581d7b28c99cd
Frankie <fwoodwar@gmail.com>
2023-09-07 22:25:22 -0600

    created a toggle to switch the mode of my background colors

--- functions.js
+++ functions.js
@@ -17,3 +17,19 @@ enterBtn.addEventListener('click', function() {
     window.location.href = newPageUrl;
 });
 
+
+const dark_modeBtn = document.querySelector('.dark_mode');
+const body = document.body;
+const mainSection = document.querySelector('.main_section');
+
+const darkGradient = 'linear-gradient(to bottom, rgb(7, 6, 110), rgb(17, 21, 28))';
+const mainDarkMode = 'rgba(182, 192, 219, 0.73)';
+const mainBorder = 'rgba(71, 115, 209, 0.73)';
+// border-color: rgb(106, 106, 141);
+// background-color: rgba(71, 115, 209, 0.73)
+
+dark_modeBtn.addEventListener('click', function() {
+    body.style.backgroundImage = darkGradient;
+    mainSection.style.backgroundColor = mainDarkMode;
+   mainSection.style.borderColor = mainBorder;
+});
